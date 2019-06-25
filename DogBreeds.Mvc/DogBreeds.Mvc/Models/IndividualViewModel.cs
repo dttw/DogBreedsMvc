d0f3aa0cf6bd74e3c1b05a488aae9c7433df92ed
Graham Whittenham <whittenhamg@gmail.com>
2019-06-25 20:01:18 +0100

    Added viewmodels for Individuals and Pictures.

---
+++ DogBreeds.Mvc/DogBreeds.Mvc/Models/IndividualViewModel.cs
@@ -0,0 +1,47 @@
+﻿using DogBreeds.Mvc.Dal.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogBreeds.Mvc.Models
+{
+    public class IndividualViewModel
+    {
+        public IndividualViewModel() { }
+
+        public IndividualViewModel(Individual individual)
+            : this(individual.Id, individual.Name, individual.BreedId)
+        {
+        }
+
+        public IndividualViewModel(string name, int breedId)
+        {
+            Name = name;
+            BreedId = breedId;
+        }
+
+        public IndividualViewModel(int id, string name, int breedId)
+        {
+            Id = id;
+            Name = name;
+            BreedId = breedId;
+        }
+
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "{0} is required.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Breed")]
+        [Required(ErrorMessage = "{0} is required.")]
+        public int BreedId { get; set; }
+
+        public IEnumerable<Breed> Breeds { get; set; } = new List<Breed>();
+
+        [Display(Name = "Pictures")]
+        public IEnumerable<Picture> Pictures { get; set; } = new List<Picture>();
+    }
+}
---
+++ DogBreeds.Mvc/DogBreeds.Mvc/Models/PictureViewModel.cs
@@ -0,0 +1,40 @@
+﻿using DogBreeds.Mvc.Dal.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogBreeds.Mvc.Models
+{
+    public class PictureViewModel
+    {
+        public PictureViewModel() { }
+
+        public PictureViewModel(Picture picture) : this(picture.Id, picture.Uri, picture.IndividualId, picture.BreedId)
+        {
+        }
+
+        public PictureViewModel(int id, string uri, int? individualId, int? breedId)
+        {
+            Id = id;
+            Uri = uri;
+            IndividualId = individualId;
+            BreedId = breedId;
+        }
+
+        public int Id { get; set; }
+
+        [Display(Name = "Location")]
+        [Required(ErrorMessage = "{0} is required.")]
+        public string Uri { get; set; }
+
+        public int? IndividualId { get; set; }
+
+        public Individual Individual { get; set; }
+
+        public int? BreedId { get; set; }
+
+        public Breed Breed { get; set; }
+    }
+}
