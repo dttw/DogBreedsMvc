94e2a3601ae6962829664c236a1b5426914ba922
Graham Whittenham <whittenhamg@gmail.com>
2019-07-07 23:31:19 +0100

    Added data loader for loading of json data into the breeds table.

---
+++ DogBreeds.Mvc/DogBreeds.Mvc/Dal/DataLoader.cs
@@ -0,0 +1,106 @@
+﻿using DogBreeds.Mvc.Controllers;
+using DogBreeds.Mvc.Dal.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogBreeds.Mvc.Dal
+{
+    public class DataLoader
+    {
+        private readonly DogBreedsContext context;
+        private TextInfo textInfo = new CultureInfo("en-GB", false).TextInfo;
+
+        public DataLoader(DogBreedsContext context) => this.context = context;
+
+        public void ImportBreeds()
+        {
+            byte[] fileContents = Json.Breeds.InitialBreeds;
+
+            ImportBreeds(fileContents);
+        }
+
+        public void ImportBreeds(byte[] fileContents)
+        {
+            if (fileContents.Length == 0)
+            {
+                throw new ArgumentException("The file containing the initial Breeds is empty.");
+            }
+
+            string json = GetJson(fileContents);
+
+            try
+            {
+                Dictionary<string, string[]> breeds = GetBreedsFromJson(json);
+
+                if (breeds != null && breeds.Count() > 0)
+                {
+                    AddBreeds(breeds);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new ArgumentException("The JSON provided does not match the required format.");
+            }            
+        }
+
+        private string GetJson(byte[] fileContents) => Encoding.UTF8.GetString(fileContents, 0, fileContents.Length);
+
+        private Dictionary<string, string[]> GetBreedsFromJson(string json) => JsonConvert.DeserializeObject<Dictionary<string, string[]>>(json);
+
+        private void AddBreeds(Dictionary<string, string[]> breeds)
+        {          
+            foreach (KeyValuePair<string, string[]> breed in breeds)
+            {
+                if (!string.IsNullOrEmpty(breed.Key))
+                {
+                    string properName = textInfo.ToTitleCase(breed.Key);
+
+                    AddBreed(new Breed(properName));
+
+                    if (breed.Value.Count() > 0)
+                    {
+                        AddChildBreeds(properName, breed.Value);
+                    }                   
+                }
+            }
+        }
+
+        private void AddBreed(Breed breed)
+        {
+            if (!BreedExists(breed.Name))
+            {
+                context.Breeds.Add(breed);
+                context.SaveChanges();
+            }           
+        }
+
+        private void AddChildBreeds(string parentBreedName, string[] childBreeds)
+        {
+            Breed parentBreed = context.Breeds.SingleOrDefault(b => b.Name == parentBreedName);
+
+            if (parentBreed != null)
+            {
+                AddChildBreeds(parentBreed, childBreeds);
+            }      
+        }
+
+        private void AddChildBreeds(Breed parentBreed, string[] childBreeds)
+        {
+            foreach (string breed in childBreeds)
+            {
+                if (!string.IsNullOrEmpty(breed))
+                {
+                    AddBreed(new Breed(textInfo.ToTitleCase(breed)) { BreedId = parentBreed.Id });
+                }                
+            }
+        }
+
+        private bool BreedExists(string name) => context.Breeds.Any(b => b.Name == name);
+    }
+}
---
+++ DogBreeds.Mvc/DogBreeds.Test/DataImport/DataLoaderTests.cs
@@ -0,0 +1,92 @@
+﻿using DogBreeds.Mvc.Dal;
+using DogBreeds.Mvc.Dal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace DogBreeds.Test.DataImport
+{
+    public class DataLoaderTests
+    {
+        [Fact]
+        public void ImportBreedsValid()
+        {
+            using (var database = new TestDb())
+            {
+                var loader = new DataLoader(database.Context);
+
+                Assert.False(database.Context.Breeds.Any(b => b.Name == "Affenpinscher"));
+                Assert.False(database.Context.Breeds.Any(b => b.Name == "African"));
+
+                loader.ImportBreeds(Encoding.ASCII.GetBytes(@"{""affenpinscher"": [], ""african"": [] }"));
+
+                Assert.True(database.Context.Breeds.Any(b => b.Name == "Affenpinscher"));
+                Assert.True(database.Context.Breeds.Any(b => b.Name == "African"));
+            }
+        }
+
+        [Fact]
+        public void ImportCreatedWithParentBreed()
+        {
+            using (var database = new TestDb())
+            {
+                var loader = new DataLoader(database.Context);
+
+                loader.ImportBreeds(Encoding.ASCII.GetBytes(@"{""bulldog"": [""boston"", ""french""]}"));
+
+                Breed bulldog = database.Context.Breeds.SingleOrDefault(b => b.Name == "Bulldog");
+
+                Assert.NotNull(bulldog);
+
+                Breed boston = database.Context.Breeds.SingleOrDefault(b => b.Name == "Boston");
+
+                Assert.NotNull(boston);
+
+                Assert.Equal(boston.BreedId, bulldog.Id);
+            }
+        }
+
+        [Fact]
+        public void ImportBreedsIgnoreDuplicate()
+        {
+            string json = @"{""bullterrier"": [], ""bullterrier"": [], ""bullterrier"": [], ""bullterrier"": [] }";
+
+            using (var database = new TestDb())
+            {
+                var loader = new DataLoader(database.Context);
+
+                loader.ImportBreeds(Encoding.ASCII.GetBytes(json));
+
+                Assert.Equal(1, database.Context.Breeds.Count(b => b.Name == "Bullterrier"));
+            }
+        }
+
+        [Fact]
+        public void ImportBreedsEmptyFile()
+        {
+            using (var database = new TestDb())
+            {
+                var loader = new DataLoader(database.Context);
+
+                Exception ex = Assert.Throws<ArgumentException>(() => loader.ImportBreeds(Array.Empty<byte>()));
+
+                Assert.Equal("The file containing the initial Breeds is empty.", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void ImportBreedsInvalidJson()
+        {
+            using (var database = new TestDb())
+            {
+                var loader = new DataLoader(database.Context);
+
+                Exception ex = Assert.Throws<ArgumentException>(() => loader.ImportBreeds(Encoding.ASCII.GetBytes(@"{""affenpinscher"": [], ""african"": [],")));
+                    
+                Assert.Equal("The JSON provided does not match the required format.", ex.Message);
+            }
+        }
+    }
+}
